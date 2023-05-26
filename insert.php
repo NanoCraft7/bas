2a29e3f3bb9bd246498591ae38cc44a25fdae62f
NanoCraft7 <125359539+NanoCraft7@users.noreply.github.com>
2023-05-26 09:05:38 +0200

    a

--- insert.php
+++ insert.php
@@ -1,36 +1,53 @@
 <?php
 require_once 'classes/userclass.php';
 
-$user = new UserClass();
 
-// Gegevens van het nieuwe klantformulier
-$klantnaam = $_POST['klantnaam'];
-$klantEmail = $_POST['klantEmail'];
-$klantAdres = $_POST['klantAdres'];
-$klantPostcode = $_POST['klantPostcode'];
-$klantWoonplaats = $_POST['klantWoonplaats'];
+    if (isset($_POST["submit"])) {
+        $user = new UserClass();
+        
+        // Gegevens van het nieuwe klantformulier
+        $klantnaam = $_POST['klantnaam'];
+        $klantEmail = $_POST['klantEmail'];
+        $klantAdres = $_POST['klantAdres'];
+        $klantPostcode = $_POST['klantPostcode'];
+        $klantWoonplaats = $_POST['klantWoonplaats'];
+        
+        // Toevoegen van een nieuwe klant met behulp van de UserClass
+        $user->addUser($klantnaam, $klantEmail, $klantAdres, $klantPostcode, $klantWoonplaats);
 
-// Toevoegen van een nieuwe klant met behulp van de UserClass
-$user->addUser($klantnaam, $klantEmail, $klantAdres, $klantPostcode, $klantWoonplaats);
+        echo "De klant is succesvol toegevoegd aan de database.";
+    } else {
+        echo "Niet alle vereiste velden zijn ingevuld.";
+    }
+}
 ?>
 
 
 <!-- HTML-formulier om een nieuwe klant toe te voegen -->
-<form method="POST" action="">
-    <label for="klantnaam">Klantnaam:</label>
-    <input type="text" name="klantnaam" required><br>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Nieuwe klant toevoegen</title>
+    </head>
+    <body>
+        <h1>Nieuwe klant toevoegen</h1>
+        <form method="POST" action="insert.php">
+            <label for="klantnaam">Klantnaam:</label>
+            <input type="text" id="klantnaam" name="klantnaam" required><br>
 
-    <label for="klantEmail">E-mailadres:</label>
-    <input type="email" name="klantEmail" required><br>
+            <label for="klantEmail">Email:</label>
+            <input type="email" id="klantEmail" name="klantEmail" required><br>
 
-    <label for="klantAdres">Adres:</label>
-    <input type="text" name="klantAdres" required><br>
+            <label for="klantAdres">Adres:</label>
+            <input type="text" id="klantAdres" name="klantAdres" required><br>
 
-    <label for="klantPostcode">Postcode:</label>
-    <input type="text" name="klantPostcode" pattern="[0-9]{4}[A-Za-z]{2}" required><br>
+            <label for="klantPostcode">Postcode:</label>
+            <input type="text" id="klantPostcode" name="klantPostcode" required><br>
 
-    <label for="klantWoonplaats">Woonplaats:</label>
-    <input type="text" name="klantWoonplaats" required><br>
+            <label for="klantWoonplaats">Woonplaats:</label>
+            <input type="text" id="klantWoonplaats" name="klantWoonplaats" required><br>
 
-    <input type="submit" value="Opslaan">
-</form>
+            <input type="submit" value="Toevoegen">
+        </form>
+    </body>
+</html>

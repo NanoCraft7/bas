36edf140a9ab9c785fabaa2215a97a9e883c4514
NanoCraft7 <125359539+NanoCraft7@users.noreply.github.com>
2023-06-05 15:14:43 +0200

    index.php preparations for future

--- index.php
+++ index.php
@@ -7,11 +7,53 @@
 <body>
     <h1>Menu</h1>
     <ul class="menu">
-        <li><a href="klant_insert.php">Klanten.</a></li>
-        <li><a href="verkooporders.php">Verkoop order lijst.</a></li>
-        <li><a href="verkooporder_insert.php">Verkoop order maken.</a></li>
-        <li><a href="inkooporders.php">Inkoop order lijst.</a></li>
-        <li><a href="inkooporder_insert.php">Inkoop order maken.</a></li>
+        <li class="main">
+            Klant beheren
+            <ul>
+                <li><a href="klant_insert.php">Klant toevoegen</a></li>
+                <li><a href="klant_zien.php">Klant zien</a></li>
+                <li><a href="klant_updaten.php">Klant updaten</a></li>
+                <li><a href="klant_verwijderen.php">Klant verwijderen</a></li>
+            </ul>
+        </li>
+        <li class="main">
+            Artikel beheren
+            <ul>
+                <li><a href="artikel_toevoegen.php">Artikel toevoegen</a></li>
+                <li><a href="artikel_zien.php">Artikel zien</a></li>
+                <li><a href="artikel_updaten.php">Artikel updaten</a></li>
+                <li><a href="artikel_verwijderen.php">Artikel verwijderen</a></li>
+            </ul>
+        </li>
+        <li class="main">
+            Leveranciers beheren
+            <ul>
+                <li><a href="leverancier_toevoegen.php">Leverancier toevoegen</a></li>
+                <li><a href="leverancier_zien.php">Leverancier zien</a></li>
+                <li><a href="leverancier_updaten.php">Leverancier updaten</a></li>
+                <li><a href="leverancier_verwijderen.php">Leverancier verwijderen</a></li>
+            </ul>
+        </li>
+    </ul><br>
+    <ul class="menu">
+        <li class="main">
+            In-/Verkoop orders beheren
+            <ul>
+                <li><a href="verkooporder_insert.php">Verkoop aanmaken</a></li>
+                <li><a href="verkooporders.php">Verkoop zien</a></li>
+                <li><a href="verkooporder_updaten.php">Verkoop updaten</a></li>
+                <li><a href="verkooporder_verwijderen.php">Verkoop verwijderen</a></li>
+            </ul>
+        </li>
+        <li class="main">
+             <br>
+            <ul>
+                <li><a href="inkooporder_insert.php">Inkoop aanmaken</a></li>
+                <li><a href="inkooporders.php">Inkoop zien</a></li>
+                <li><a href="inkooporder_updaten.php">Inkoop updaten</a></li>
+                <li><a href="inkooporder_verwijderen.php">Inkoop verwijderen</a></li>
+            </ul>
+        </li>
     </ul>
 </body>
 </html>
